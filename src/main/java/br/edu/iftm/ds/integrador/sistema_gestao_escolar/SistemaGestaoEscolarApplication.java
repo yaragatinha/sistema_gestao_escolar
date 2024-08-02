1ad09e2dedfb72f7e77f8df1181a579c1c5aa2ce
yaragatinha <yaragoliveira21@outlook.com.br>
2024-08-02 00:28:05 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/ds/integrador/sistema_gestao_escolar/SistemaGestaoEscolarApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.integrador.sistema_gestao_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/ds/integrador/sistema_gestao_escolar/SistemaGestaoEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.ds.integrador.sistema_gestao_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
